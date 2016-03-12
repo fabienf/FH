5e5bcee3debe23737630e7223e804f56c29bdcf2
franciscovargas <vargfran@gmail.com>
2016-03-12 14:54:57 +0000

    image analysis happening in backend. Absolutely horrendous unfactored and unstructured code sozzles

--- project/server/main/views.py
+++ project/server/main/views.py
@@ -22,13 +22,46 @@ main_blueprint = Blueprint('main', __name__,)
 ################
 
 
+
+########### Python 2 #############
+import httplib as httplib, urllib as urllib, base64
+import json
+
+# Soz for this blasphemy :'( its the need for speed #YouOnlyFacebookOnce
+
+class VisionAPI:
+    headers = {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': '4211f4e020b74e23b73be45672d4f2c7',
+    }
+
+    body = {"URL": None}
+
+    def __init__(self):
+        pass
+
+    def get_json(self, url):
+        self.body["URL"] = url
+        conn = httplib.HTTPSConnection('api.projectoxford.ai')
+        conn.request("POST", "/emotion/v1.0/recognize", json.dumps(self.body) , self.headers)
+        response = conn.getresponse()#
+        data = response.read()
+        # print(data)
+        conn.close()
+        return data
+
+
+
+
+
 @main_blueprint.route('/', methods=['GET', 'POST'])
 # @user_blueprint.route('/register')
 
 def home():
     print request.form
     if request.method == 'POST':
-         print "TEEEEEST"
+         vap = VisionAPI()
+         print vap.get_json(request.form["img"])
     return render_template('main/home.html')
 
 
