85572d0189bd515b5f0316fd03a50da7d0baf7a9
marekstrelec <marek.strelec@gmail.com>
2016-03-13 05:14:59 +0000

    category analysis

--- data_analysis/analyse.py
+++ data_analysis/analyse.py
@@ -26,28 +26,55 @@ class Analyse:
         taxonomies = []
         for n in self.data:
             taxonomy = n['alchemy']['taxonomy']
-            taxonomies.append(taxonomy)
+            taxonomies.append((taxonomy, n['real_url']))
 
         return taxonomies
+
+    def get_category_info(self):
+        c = self.get_taxonomy()
+
+        taxonomies = []
+        for n in c:
+            for m in n[0]:
+                taxonomies.append((m['label'], n[1]))
+
+        categories = {}
+        for n, m in taxonomies:
+            primary = n.split('/')[1]
+            if len(n.split('/')) > 2:
+                secondary = n.split('/')[2]
+            else:
+                secondary = None
+            if primary not in categories:
+                categories[primary] = {
+                    'all_count': 0,
+                    'secondaries': dict()
+                }
+
+            if secondary and secondary not in categories[primary]['secondaries']:
+                categories[primary]['secondaries'][secondary] = {
+                    'count': 0,
+                    'urls': []
+                }
+
+            categories[primary]['all_count'] += 1
+
+            try:
+                if secondary:
+                    categories[primary]['secondaries'][secondary]['count'] += 1
+                    categories[primary]['secondaries'][secondary]['urls'].append(m)
+            except Exception, e:
+                raise e
+
+        return categories
+
+    def find_specific_category(self, primary, secondary):
+        categories = self.get_category_info()
+        return categories[primary]['secondaries'][secondary]
 
 if __name__ == "__main__":
     file_name = "../extractor/output/bbac_1150_all.json"
     a = Analyse(file_name)
-    c = a.get_taxonomy()
-
-    taxonomies = []
-    for n in c:
-        for m in n:
-            taxonomies.append(m['label'])
-    z = Counter(taxonomies)
-    sorted_z = sorted(z.items(), key=operator.itemgetter(1))
-
-    categories = {}
-    for n in z:
-        main = n.split('/')[1]
-        if main not in categories:
-            categories[main] = 0
-
-        categories[main] += z[n]
+    b = a.find_specific_category('art and entertainment', 'movies')
 
     embed()
